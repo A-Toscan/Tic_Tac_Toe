e3a8fb0f7f1e9309af40fa1a803a27a74b33f2d8
Toscan <dipointg@gmail.com>
2023-04-01 15:03:02 +0200

    changing the winner javascript page for the draw function

--- js/winner.js
+++ js/winner.js
@@ -1,114 +1,128 @@
 //   WINNER SECTION
 
-//Función de estructura HTML  Winner
-// const getWinnerHTML = (game) => {
-//   return `
-//     <div class="winner-box">
-//     <div class="winner">
-//     <h2 class="winnerName">${game.winner} Won The Game</h2>
-//     <div class="button-player">
-//         <button id="winner-button">Play Again</button>
-//     </div>
-//     </div>
-//     </div>`;
-// };
-
-// const renderWinner = () => {
-//   container.innerHTML = getWinnerHTML(game);
-//   console.log("winner");
-// };
-
-// const setWinnerUiEventos = () => {
-//   document.getElementById("winner-button").addEventListener("click", () => {
-//     renderWelcome();
-//     setWelcomeUiEventos();
-//   });
-// };
-
 const renderWinner = (playerWinner) => {
-  return `<div class="winner-box">
+  return `
+  <div class="winner-box">
   <div class="winner">
   <h2 class="winnerName">${playerWinner} Won The Game</h2>
-  <div class="button-player">
+  <div class="button-player" id="button-play-again">
       <button id="winner-button">Play Again</button>
   </div>
   </div>
   </div>`;
 };
 
-
-
-/* Pensar como refactorizar el codigo de checkWinner para poder hacer 
-el if de mapBoxes[0] == player (como cambiar el codigo para que la condicion 
-sea eventualmente True)*/
-
-const checkWinner = (player, mapBoxes) => {
-  console.log("check-winner");
-  const uiRenderWinner = () => {
-  console.log("render-winner");
-
-    container.innerHTML = renderWinner(player);
-  };
-  console.log(mapBoxes[0]);
-  if (mapBoxes[0] == player && mapBoxes[1] == player && mapBoxes[2] == player) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[3] == player &&
-    mapBoxes[4] == player &&
-    mapBoxes[5] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[6] == player &&
-    mapBoxes[7] == player &&
-    mapBoxes[8] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[0] == player &&
-    mapBoxes[3] == player &&
-    mapBoxes[6] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[1] == player &&
-    mapBoxes[4] == player &&
-    mapBoxes[7] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[2] == player &&
-    mapBoxes[5] == player &&
-    mapBoxes[8] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[0] == player &&
-    mapBoxes[4] == player &&
-    mapBoxes[8] == player
-  ) {
-    uiRenderWinner();
-  } else if (
-    mapBoxes[2] == player &&
-    mapBoxes[4] == player &&
-    mapBoxes[6] == player
-  ) {
-    uiRenderWinner();
-  }
+const renderDraw = () => {
+  return `
+  <div class="draw-box">
+  <div class="draw">
+  <h2 class="winnerDraw">The Game was draw</h2>
+  <div class="button-player" id="button-play-again">
+      <button id="draw-button">Play Again</button>
+  </div>
+  </div>
+  </div>`;
 };
 
-const checkDraw = (mapBoxes) => {
+// CHECK WINNER / DRAW FUNCTIONALITY
+
+const checkWinner = (playerMark, mapBoxes) => {
+  const uiRenderWinner = () => {
+    if (playerMark == "X") {
+      container.innerHTML = renderWinner(
+        playersBtn.player1.name,
+        playersBtn.player1.mark
+      );
+    } else if (playerMark == "O") {
+      container.innerHTML = renderWinner(
+        playersBtn.player2.name,
+        playersBtn.player2.mark
+      );
+    }
+
+    const winnerButton = document.getElementById("winner-button");
+    winnerButton.addEventListener("click", () => {
+      renderIndex();
+      renderBoard();
+    });
+
+    const drawButton = document.getElementById("draw-button");
+    drawButton.addEventListener("click", () => {
+      renderIndex();
+      renderBoard();
+    });
+  };
+  console.log("check Winner");
+  console.log(playerMark);
+  console.log(mapBoxes);
   if (
-    mapBoxes[0] != undefined &&
-    mapBoxes[1] != undefined &&
-    mapBoxes[2] != undefined &&
-    mapBoxes[3] != undefined &&
-    mapBoxes[4] != undefined &&
-    mapBoxes[5] != undefined &&
-    mapBoxes[6] != undefined &&
-    mapBoxes[7] != undefined &&
-    mapBoxes[8] != undefined
+    mapBoxes[0] == playerMark &&
+    mapBoxes[1] == playerMark &&
+    mapBoxes[2] == playerMark
   ) {
-    container.innerHTML = renderWinner("The Game is draw!");
+    uiRenderWinner();
+  } else if (
+    mapBoxes[3] == playerMark &&
+    mapBoxes[4] == playerMark &&
+    mapBoxes[5] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[6] == playerMark &&
+    mapBoxes[7] == playerMark &&
+    mapBoxes[8] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[0] == playerMark &&
+    mapBoxes[3] == playerMark &&
+    mapBoxes[6] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[1] == playerMark &&
+    mapBoxes[4] == playerMark &&
+    mapBoxes[7] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[2] == playerMark &&
+    mapBoxes[5] == playerMark &&
+    mapBoxes[8] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[0] == playerMark &&
+    mapBoxes[4] == playerMark &&
+    mapBoxes[8] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[2] == playerMark &&
+    mapBoxes[4] == playerMark &&
+    mapBoxes[6] == playerMark
+  ) {
+    uiRenderWinner();
+  } else if (
+    mapBoxes[0] != "" &&
+    mapBoxes[1] != "" &&
+    mapBoxes[2] != "" &&
+    mapBoxes[3] != "" &&
+    mapBoxes[4] != "" &&
+    mapBoxes[5] != "" &&
+    mapBoxes[6] != "" &&
+    mapBoxes[7] != "" &&
+    mapBoxes[8] != ""
+  ) {
+    const uiXTurn = document.getElementById("X-Turn");
+    const uiOTurn = document.getElementById("O-Turn");
+
+    container.innerHTML = renderDraw();
+    uiXTurn.style.display = "none";
+    uiOTurn.style.display = "none";
+    const restartButton = document.getElementById("draw-button");
+    restartButton.addEventListener("click", () => {
+      renderIndex();
+    });
   }
 };
